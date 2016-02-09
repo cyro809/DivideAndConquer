52c569a34ff8fdf11afa1e18224ca9307c058cb0
cyro809 <cyro809@gmail.com>
2016-02-09 15:11:58 -0200

    Divisão básica dos pontos em vetores menores.

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+using namespace std;
+
+class Point
+{
+    public:
+        Point(double px, double py){ x = px; y = py; }
+        Point() {x = 0; y = 0; }
+        void print(){ cout<<"("<<x<<","<<y<<")"<<endl; }
+    private:
+        double x;
+        double y;
+
+};
+
+void divideAndConquer(Point *points, int points_length)
+{
+    if(points_length > 1)
+    {
+        int ha_length = points_length/2;
+        Point ha[ha_length];
+        Point *hb;
+
+        int hb_length = points_length/2;
+        if(points_length%2 == 0)
+        {
+            hb = new Point[hb_length];
+        }
+        else
+        {
+            hb_length++;
+            hb = new Point[hb_length];
+        }
+
+
+        int b = 0;
+        for(int i=0;i<points_length;i++)
+        {
+            if(i < points_length/2)
+            {
+                ha[i] = points[i];
+                ha[i].print();
+            }
+            else
+            {
+                hb[b] = points[i];
+                hb[b].print();
+                b++;
+            }
+        }
+        cout<<endl;
+        divideAndConquer(ha,ha_length);
+
+        cout<<endl;
+        divideAndConquer(hb,hb_length);
+    }
+    else
+    {
+        cout<<endl;
+        points[0].print();
+    }
+
+
+
+}
+
+int main()
+{
+    Point ps[] = {Point(1,2), Point(2,2), Point(2,1), Point(1,1), Point(3,3)};
+    int length = 5;
+    divideAndConquer(ps,length);
+    return 0;
+}
